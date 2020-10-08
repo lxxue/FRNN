b54e57fafeb89578843bd92eb03dbbfc851f2528
Lixin <lixxue@ethz.ch>
2020-10-08 21:02:20 +0200

    upload data; frnn validation clean done;

--- tests/frnn_validation.py
+++ tests/frnn_validation.py
@@ -10,33 +10,15 @@ num_points_fixed_query = 100000
 
 class TestFRNN:
   def __init__(self, fname, num_pcs=1, K=5, r=0.1, same=False):
-    if 'random' in fname:
-      # fname format: random_{num_points}
-      num_points = int(fname.split('_')[1])
-      pc1 = torch.rand((num_pcs, num_points, 3), dtype=torch.float)
-      pc2 = torch.rand((num_pcs, num_points, 3), dtype=torch.float)
-      for i in range(num_pcs):
-        for j in range(3):
-          pc1[i, :, j] *= torch.rand(1)+0.5
-          pc2[i, :, j] *= torch.rand(1)+0.5
-    else:
-      pc1 = torch.FloatTensor(read_ply(fname)[None, :, :3])  # no need for normals
-      # pc2 = pc1
-      pc2 = torch.FloatTensor(read_ply(fname)[None, :, :3])  # no need for normals
-      pc1 = normalize_pc(pc1)
-      pc2 = normalize_pc(pc2)
-      # print("pc1 bbox: ", pc1.min(dim=1)[0], pc1.max(dim=1)[0])
-      num_points = pc2.shape[1]
-      if num_pcs > 1:
-        pc1 = pc1.repeat(num_pcs, 1, 1)
-        pc2 = pc2.repeat(num_pcs, 1, 1)
-
+    pc1 = torch.load(fname)
+    pc2 = torch.load(fname)
     if not same:
       pc1 = torch.rand((num_pcs, num_points_fixed_query, 3), dtype=torch.float)
     self.num_pcs = num_pcs
     self.fname = fname.split('/')[-1]
     self.K = K
     self.r = r
+    num_points = pc2.shape[1]
     self.num_points = num_points
     self.same = same
     self.pc1_knn = pc1.clone().detach().cuda()
@@ -60,7 +42,7 @@ class TestFRNN:
     self.lengths2_cuda = lengths2.cuda()
     print("{}: #points: {}".format(self.fname, num_points))
     self.grid = None
-    self.grad_dists = torch.ones((num_pcs, pc1.shape[0], K), dtype=torch.float32).cuda()
+    # self.grad_dists = torch.ones((num_pcs, pc1.shape[0], K), dtype=torch.float32).cuda()
 
   def frnn_grid(self):
     dists, idxs, nn, grid = frnn.frnn_grid_points(
@@ -104,14 +86,10 @@ class TestFRNN:
       return_sorted=True
     ) 
     # for backward, assume all we have k neighbors within the radius
-    mask = dists > self.r * self.r
-    idxs[mask] = -1
-    dists[mask] = -1
-    nn[mask] = 0.
-
-    # print(dists.shape)
-    # print(mask.shape)
-    # print(idxs.shape)
+    # mask = dists > self.r * self.r
+    # idxs[mask] = -1
+    # dists[mask] = -1
+    # nn[mask] = 0.
     return dists, idxs, nn
 
   # def frnn_bf(self):
@@ -126,14 +104,17 @@ class TestFRNN:
   #   return dists, idxs
 
   def compare_frnn_knn(self):
-    if self.num_points > 10000000:
-      print("\tnumber of points for exceed 1 million; skip")
-      return None
-
     # forward    
     dists_knn, idxs_knn, nn_knn = self.knn()
     dists_frnn, idxs_frnn, nn_frnn = self.frnn_grid()
     dists_frnn_reuse, idxs_frnn_reuse, nn_frnn_reuse = self.frnn_grid_reuse()
+
+    # modify results from knn to make it match frnn
+    mask = idxs_frnn == -1
+    idxs_knn[mask] = -1
+    dists_knn[mask] == dists_frnn[mask]
+    nn_knn[mask[..., None].expand(-1,-1,-1,3)] = nn_frnn[mask[..., None].expand(-1,-1,-1,3)]
+
     # dists_frnn_bf, idxs_frnn_bf = self.frnn_bf()
 
     # backward
@@ -143,11 +124,6 @@ class TestFRNN:
     # loss_frnn.backward()
     # loss_frnn_reuse = (dists_frnn_reuse * self.grad_dists).sum()
     # loss_frnn_reuse.backward()
-
-    # for i in range(self.pc2_knn.shape[1]):
-    #   if not torch.allclose(self.pc2_knn.grad[0, i], self.pc2_frnn.grad[0, i]):
-    #     print(self.pc2_knn.grad[0, i])
-    #     print(self.pc2_frnn.grad[0, i])
 
     idxs_all_same = torch.all(idxs_frnn == idxs_knn).item()
     idxs_all_same_reuse = torch.all(idxs_frnn_reuse == idxs_knn).item()
@@ -171,17 +147,6 @@ class TestFRNN:
     #         dists_all_close, dists_all_close_reuse, nn_all_close, nn_all_close_reuse, pc1_grad_all_close,
     #         pc1_grad_all_close_reuse, pc2_grad_all_close, pc2_grad_all_close_reuse]
 
-  # def compare_frnnreuse_knn(self):
-  #   if self.num_points > 10000000:
-  #     print("\tnumber of points for exceed 1 million; skip")
-  #     return None
-  #   dists_knn, idxs_knn, nn_knn = self.knn()
-  #   dists_frnn, idxs_frnn, nn_frnn = self.frnn_grid_reuse()
-
-  #   diff_keys_percentage = torch.sum(idxs_frnn == idxs_knn).type(torch.float).item() / self.K / self.pc1_cuda.shape[1] / self.num_pcs
-  #   dists_all_close = torch.allclose(dists_frnn, dists_knn)
-  #   nn_all_close = torch.allclose(nn_frnn, nn_knn)
-  #   return [self.fname, self.num_points, "{:.4f}".format(diff_keys_percentage), dists_all_close, nn_all_close]
 
 def normalize_pc(pc):
   # convert pc to the unit box so that we don't need to manually set raidus for each mesh
@@ -195,15 +160,14 @@ def normalize_pc(pc):
 
 
 if __name__ == "__main__":
-  fnames = sorted(glob.glob('data/*.ply') + glob.glob('data/*/*.ply'))
-  fnames += ['random_10000', 'random_100000', 'random_1000000']
+  # fnames = sorted(glob.glob('data/*.ply') + glob.glob('data/*/*.ply'))
+  # fnames += ['random_10000', 'random_100000', 'random_1000000']
+  fnames = sorted(glob.glob("data/pc/*.pt"))
   print(fnames)
   with open("tests/output/frnn_validation.csv", 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Point cloud', 'Num points', 'Different key percentage', 'Dists all close'])
     for fname in fnames:
-      if 'xyz' in fname or 'lucy' in fname:
-        continue
       validator = TestFRNN(fname, same=False)
       results = validator.compare_frnn_knn()
       print(results)
